4fbb65d428cff0af761f2f9b71f51ef9e7b0c057
Matthew Duphily <matthew.duphily@live.com>
2019-09-22 15:55:47 -0400

    Initial commit

---
+++ app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.roasted715jr.colorblindassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
