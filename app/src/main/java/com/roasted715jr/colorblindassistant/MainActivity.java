ba60dabfa2b99fb49faddefd47bd467ee424825d
Matthew Duphily <matthew.duphily@live.com>
2019-09-22 16:59:51 -0400

    Imported openCV

--- app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
+++ app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
@@ -1,6 +1,7 @@
 package com.roasted715jr.colorblindassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
