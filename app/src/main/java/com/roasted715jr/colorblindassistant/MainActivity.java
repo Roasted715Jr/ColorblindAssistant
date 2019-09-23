9a4374d37adbdc40c45f29197adb6998d440857c
Matthew Duphily <matthew.duphily@live.com>
2019-09-22 23:21:11 -0400

    Phone can take and save picture

--- app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
+++ app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
@@ -1,21 +1,40 @@
 package com.roasted715jr.colorblindassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 //https://developer.android.com/training/camera/photobasics
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    Bitmap imageBitmap;
     Button btnTakePicture;
     ImageView imgThumbnail;
+    String currentPhotPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +50,48 @@ public class MainActivity extends AppCompatActivity {
                 dispatchTakePictureIntent();
             }
         });
+
+
+//        imgThumbnail.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                int x = (int) event.getX();
+//                int y = (int) event.getY();
+//                Log.d(TAG, "" + x + ", " + y);
+//
+//                int pixel = imageBitmap.getPixel(x, y);
+////                Log.d(TAG, String.valueOf(pixel));
+//
+//                int r = Color.red(pixel);
+//                int g = Color.green(pixel);
+//                int b = Color.blue(pixel);
+//                Log.d(TAG, r + ", " + g + ", " + b);
+//
+//                return false; //Not sure what true would do
+//            }
+//        });
     }
 
     //Get a camera app to take a picture, and request the image back
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Ensure there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            //Create file where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Only proceed if the file was successfully created
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, "com.roasted715jr.colorblindassistant", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, 0);
+                galleryAddPic();
+            }
         }
     }
 
@@ -48,8 +102,31 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
             imgThumbnail.setImageBitmap(imageBitmap);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); //This directory makes these photos private to the app by default
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        //Save a file: path for use with ACTION_VIEW intents
+        currentPhotPath = image.getAbsolutePath();
+        Log.d(TAG, "Image created and stored successfully");
+        return image;
+    }
+
+    //Add the photo to the media gallery to make available system-wide
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+        Log.d(TAG, "This jawn should be in the gallery now");
     }
 }
