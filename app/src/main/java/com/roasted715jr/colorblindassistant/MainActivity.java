72eead81e6ed89a7cc3ab95ed33c1b1cf5d7933a
Matthew Duphily <matthew.duphily@live.com>
2019-09-23 10:16:33 -0400

    Yeet we in business

--- app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
+++ app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
@@ -5,21 +5,15 @@ import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -34,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Bitmap imageBitmap;
     Button btnTakePicture;
     ImageView imgThumbnail;
-    String currentPhotPath;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,9 +82,8 @@ public class MainActivity extends AppCompatActivity {
             //Only proceed if the file was successfully created
             if (photoFile != null) {
                 Uri photoUri = FileProvider.getUriForFile(this, "com.roasted715jr.colorblindassistant", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                startActivityForResult(takePictureIntent, 0);
-                galleryAddPic();
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri); //Including this makes the thumbnail not work
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
     }
@@ -105,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
             imageBitmap = (Bitmap) extras.get("data");
             imgThumbnail.setImageBitmap(imageBitmap);
         }
+
+        galleryAddPic();
     }
 
     private File createImageFile() throws IOException {
@@ -112,21 +107,27 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); //This directory makes these photos private to the app by default
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_DCIM);
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
         //Save a file: path for use with ACTION_VIEW intents
-        currentPhotPath = image.getAbsolutePath();
+        currentPhotoPath = image.getAbsolutePath();
         Log.d(TAG, "Image created and stored successfully");
         return image;
     }
 
     //Add the photo to the media gallery to make available system-wide
     private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
+
+//        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+//        Uri contentUri = Uri.fromFile(f);
+//        mediaScanIntent.setData(contentUri);
+//        this.sendBroadcast(mediaScanIntent);
+
+        grantUriPermission("com.roasted715jr.colorblindassistant", Uri.fromFile(f), 0);
+        MediaStore.Images.Media.insertImage(getContentResolver(), imageBitmap, "Title" , "Description");
+
         Log.d(TAG, "This jawn should be in the gallery now");
     }
 }
