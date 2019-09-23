0c568e04cf97c0d10c6912323552dd0390b1d434
Matthew Duphily <matthew.duphily@live.com>
2019-09-22 20:08:28 -0400

    Camera functionality added

--- app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
+++ app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
@@ -2,13 +2,54 @@ package com.roasted715jr.colorblindassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+//https://developer.android.com/training/camera/photobasics
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Button btnTakePicture;
+    ImageView imgThumbnail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTakePicture = (Button) findViewById(R.id.btn_take_picture);
+        imgThumbnail = (ImageView) findViewById(R.id.img_thumbnail);
+
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    //Get a camera app to take a picture, and request the image back
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //Retrieve the returned thumbnail image and display it
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgThumbnail.setImageBitmap(imageBitmap);
+        }
     }
 }
