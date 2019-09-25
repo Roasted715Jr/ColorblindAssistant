4e776c87fb89a22a285fb68a40b5b43f9487fc33
Matthew Duphily <matthew.duphily@live.com>
2019-09-24 21:06:34 -0400

    App asks for camera permission

--- app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
+++ app/src/main/java/com/roasted715jr/colorblindassistant/MainActivity.java
@@ -1,9 +1,13 @@
 package com.roasted715jr.colorblindassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +17,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,6 +29,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE = 1234;
 
     Bitmap imageBitmap;
     Button btnTakePicture;
@@ -33,7 +39,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+        }
 
         btnTakePicture = (Button) findViewById(R.id.btn_take_picture);
         imgThumbnail = (ImageView) findViewById(R.id.img_thumbnail);
@@ -66,6 +78,34 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
+    //Retrieve the returned thumbnail image and display it
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imgThumbnail.setImageBitmap(imageBitmap);
+        }
+
+        galleryAddPic();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "Permission Granted");
+                } else {
+                    Log.d(TAG, "Permission Denied");
+                    Toast.makeText(this, "Camera permission required for app to work", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+        }
+    }
+
     //Get a camera app to take a picture, and request the image back
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -86,20 +126,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
-    }
-
-    //Retrieve the returned thumbnail image and display it
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            imgThumbnail.setImageBitmap(imageBitmap);
-        }
-
-        galleryAddPic();
     }
 
     private File createImageFile() throws IOException {
